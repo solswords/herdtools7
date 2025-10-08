bd1734ee71c2621868bae9ca7ed62cdd97464e14
Sol Swords <sol.swords@arm.com>
2025-10-08 09:03:34 -0500

    Initial proof of one direction of aarch64haz cat equivalence

--- acl2/cat/eval-ins.lisp
+++ acl2/cat/eval-ins.lisp
@@ -46,9 +46,9 @@
   (if (atom rels)
       nil
     (cons (change-result result :env
-                         (cons (cons (var-fix var)
-                                     (val-fix (car rels)))
-                               (result->env result)))
+                         (env-update var
+                                     (car rels)
+                                     (result->env result)))
           (withfrom-results var (cdr rels) result))))
 
 
@@ -110,7 +110,7 @@
                     ((expval env)
                      (if (function-bindings-p x.bindings)
                          (b* ((fn-env (recursive-function-bindings-to-closures x.bindings env)))
-                           (norm (append fn-env env)))
+                           (norm (env-combine fn-env env)))
                        (b* ((env (add-empty-bindings-to-env x.bindings env))
                             ((expval env) (eval-fixpoint x.bindings)))
                          (norm env))))
@@ -124,7 +124,7 @@
                       (norm (list (change-result result :flags (cons x.test.name result.flags))))))
                   (norm (list (change-result result :judgement :forbidden))))
         :i_procedure (b* ((proc (v_proc x.formals x.body result.env))
-                          (env (cons (cons x.name proc) result.env)))
+                          (env (env-update x.name proc result.env)))
                        (norm (list (change-result result :env env))))
         :i_call      (b* (((expval proc) (b* ((look (env-lookup x.proc result.env))
                                               ((unless look)
@@ -139,7 +139,7 @@
                                     (msg "in procedure call of" x.proc)))
                               ((expval rslts)
                                (eval-inslist proc.body
-                                             :result (change-result result :env (append argenv result.env))
+                                             :result (change-result result :env (env-combine argenv result.env))
                                              :reclimit (1- reclimit))))
                            (norm (resultlist-replace-env rslts result.env)))
                          :otherwise (err "Bad procedure call"
--- acl2/cat/interp-types.lisp
+++ acl2/cat/interp-types.lisp
@@ -23,6 +23,8 @@
 (in-package "CAT")
 
 (include-book "ast")
+(include-book "../asl/utils/omaps")
+(local (include-book "varset"))
 (local (table fty::deftagsum-defaults :short-names t))
 (local (std::add-default-post-define-hook :fix))
 
@@ -104,12 +106,8 @@
 
   (deflist vallist :elt-type val :true-listp t :elementp-of-nil nil)
 
-  (fty::defmap env :key-type var :val-type val :true-listp t :keyp-of-nil nil :valp-of-nil nil))
+  (fty::defomap env :key-type var :val-type val))
 
-(defthm env-p-of-append
-  (implies (and (env-p x)
-                (env-p y))
-           (env-p (append x y))))
 
 (defenum val-kind-p (:v_empty :v_tag :v_rel :v_set :v_valset :v_tuple :v_enum :v_fun :v_proc))
 (defthm val-kind-p-of-val-kind
@@ -167,13 +165,130 @@
   (define env-well-formed ((x env-p))
     :measure (env-count x)
     (b* ((x (env-fix x)))
-      (if (atom x)
+      (if (omap::emptyp x)
           t
-        (and (val-well-formed (cdar x))
-             (env-well-formed (cdr x))))))
+        (and (val-well-formed (omap::head-val x))
+             (env-well-formed (omap::tail x))))))
 
   ///
   (fty::deffixequiv-mutual well-formed))
       
    
+
+(define env-lookup ((v var-p) (env env-p))
+  :returns (val (iff (val-p val) val))
+  (cdr (omap::assoc (var-fix v) (env-fix env))))
+
+(define env-update ((var var-p) (val val-p) (env env-p))
+  :returns (new-env env-p)
+  (omap::update (var-fix var) (val-fix val) (env-fix env))
+  ///
+  (defret env-lookup-of-<fn>
+    (equal (env-lookup v new-env)
+           (if (equal (var-fix v) (var-fix var))
+               (val-fix val)
+             (env-lookup v env)))
+    :hints(("Goal" :in-theory (enable env-lookup)))))
+
+
+(define env-extract ((vars varlist-p) (env env-p))
+  :Returns (new-env env-p)
+  :prepwork ((local (defthm env-p-of-omap-restrict
+                      (implies (env-p env)
+                               (env-p (omap::restrict keys env)))
+                      :hints(("Goal" :in-theory (enable omap::restrict))))))
+  (omap::restrict (mergesort (varlist-fix vars)) (env-fix env))
+  ///
+  (defret env-lookup-of-<fn>
+    (equal (env-lookup v new-env)
+           (and (member-equal (var-fix v) (varlist-fix vars))
+                (env-lookup v env)))
+    :hints(("Goal" :in-theory (enable env-lookup
+                                      omap::assoc-of-restrict)))))
+
+
+  
+(local (defthm cdr-of-omap-assoc-env
+         (implies (env-p env)
+                  (iff (cdr (omap::assoc k env))
+                       (omap::assoc k env)))
+         :hints(("Goal" :in-theory (enable omap::assoc)))))
+
+(define env-combine ((env1 env-p) (env2 env-p))
+  :returns (new-env env-p)
+  (omap::update* (env-fix env1) (env-fix env2))
+  ///
+  (defret env-lookup-of-<fn>
+    (equal (env-lookup v new-env)
+           (or (env-lookup v env1)
+               (env-lookup v env2)))
+    :hints(("Goal" :in-theory (enable env-lookup))))
+
+  (defret env-combine-of-empty
+    (equal (env-combine nil x) (env-fix x))
+    :hints(("Goal" :in-theory (enable omap::update*)))))
+
+
+(define env-diff-key ((x env-p) (y env-p))
+  :returns (key (iff (var-p key) key))
+  :verify-guards nil
+  (let ((key (omap::diff-key (env-fix x) (env-fix y))))
+    (and key (var-fix key)))
+  ///
+  (local (defthm diff-key-x-x
+           (not (omap::diff-key x x))
+           :hints(("Goal" :in-theory (enable omap::diff-key)))))
+  (local (defthm key-var-p-when-assoc-env
+           (implies (and (env-p env)
+                         (not (var-p key)))
+                    (not (omap::assoc key env)))))
+  
+  (local (defthm car-of-omap-assoc
+           (equal (car (omap::assoc k y))
+                  (and (omap::assoc k y) k))
+           :hints(("Goal" :in-theory (enable omap::assoc)))))
+                  
+  (verify-guards env-diff-key
+    :hints (("goal" :use ((:instance omap::diff-key-when-unequal
+                           (x (env-fix x)) (y (env-fix y))))))
+    :otf-flg t)
+  
+  (defretd env-diff-key-when-unequal
+    (implies (not (equal (env-fix x) (env-fix y)))
+             (not (equal (env-lookup key x)
+                         (env-lookup key y))))
+    :hints (("goal" :use ((:instance omap::diff-key-when-unequal
+                           (x (env-fix x)) (y (env-fix y))))
+             :in-theory (enable env-lookup)
+             :cases ((var-p (omap::diff-key (env-fix x) (env-fix y)))))
+            '(:cases ((omap::assoc (env-diff-key x y) (env-fix x))))
+            '(:cases ((omap::assoc (env-diff-key x y) (env-fix y)))))))
+
+
+(define env-keys ((x env-p))
+  :returns (keys (and (setp keys)
+                      (varlist-p keys)))
+  :prepwork ((local (defthm varlist-p-keys-of-env
+                      (implies (env-p env)
+                               (varlist-p (omap::keys env)))
+                      :hints(("Goal" :in-theory (enable omap::keys))))))
+  (omap::keys (env-fix x))
+  ///
+  (defthmd in-env-keys-iff-env-lookup
+    (iff (in v (env-keys x))
+         (and (var-p v)
+              (env-lookup v x)))
+    :hints(("Goal" :in-theory (enable env-lookup
+                                      omap::in-of-keys-to-assoc))))
+
+  (defthm env-keys-of-env-update
+    (equal (env-keys (env-update v val x))
+           (insert (var-fix v) (env-keys x)))
+    :hints(("Goal" :in-theory (enable env-update))))
+
+  (defthm env-keys-of-env-combine
+    (equal (env-keys (env-combine x y))
+           (union (env-keys x) (env-keys y)))
+    :hints(("Goal" :in-theory (enable env-combine)))))
+
 
